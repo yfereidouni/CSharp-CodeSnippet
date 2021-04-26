c7b53f4f11beb59f1a194fb9570a3d88710c9eb1
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-04-26 21:42:34 +0430

    How to use Struct.

---
+++ iStructures/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace iStructures
+{
+    public struct Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public Person(string firstname,string lastname, int age)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Age = age;
+        }
+        public void Display()
+        {
+            Console.WriteLine(FirstName);
+            Console.WriteLine(LastName);
+            Console.WriteLine(Age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person yasser = new Person("Yasser", "Fereidouni", 36);
+            yasser.Display();
+        }
+    }
+}
