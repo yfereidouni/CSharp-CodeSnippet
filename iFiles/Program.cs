fbd380e3fd14b2611014e2c65c12a5344db5b49b
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-04-26 10:43:10 +0430

    Reading and Writing Files by StreamReader and StreamWriter.

--- iFiles/Program.cs
+++ iFiles/Program.cs
@@ -5,11 +5,40 @@ namespace iFiles
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            using (StreamWriter file = new StreamWriter (@"c:\ifiles.txt"))
-            {
+            //Current execution path of program
+            var GetDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
-            }        }
+            //-----------------------------------------------------------------------------
+            //Method 1 -------------------------------------------------------------------
+            //-----------------------------------------------------------------------------
+            string[] lines = { "first line.", "second line.", "third line." };
+            File.WriteAllLines("iFile.txt", lines);
+            var text = File.ReadAllText("iFile.txt");
+            Console.WriteLine(text);
+            //-----------------------------------------------------------------------------
+
+            //-----------------------------------------------------------------------------
+            //Method 2 --------------------------------------------------------------------
+            //-----------------------------------------------------------------------------
+            using (StreamWriter writer = new StreamWriter("iFile.txt"))
+            {
+                writer.WriteLine("Hello");
+                writer.WriteLine("Yasser Fereidouni");
+                writer.WriteLine("09124643426");
+            }
+            using (StreamReader reader = new StreamReader("iFile.txt"))
+            {
+                //Console.WriteLine(reader.ReadToEnd());
+                while (!reader.EndOfStream)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                }
+            }
+            //-----------------------------------------------------------------------------
+        }
     }
 }
