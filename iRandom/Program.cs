9851c0fdcab9530964151dc8dcf8632ffaf75140
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-04-27 13:40:05 +0430

    Regular Expression Usage.

---
+++ iRandom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace iRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int numEyes;
+
+            //Generating 10 random number in range of 1 to 7 (notice: 7 is exclude)
+            for(int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 7);
+                Console.WriteLine(numEyes);
+            }
+        }
+    }
+}
---
+++ iRegularExpression/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace iRegularExpression
+{
+    //Reference for Regex: ----------------------------------------------------------------------------------
+    //https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+    //-------------------------------------------------------------------------------------------------------
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // defining a regular expression with a pattern
+            Regex regex = new Regex(@"\d");
+
+            // Test string
+            string text = "Hi there 123";
+
+            // Find hits
+            MatchCollection hits = regex.Matches(text);
+
+            // Anzahl der Treffer
+            Console.WriteLine("{0} hits found:\n   {1}",
+                              hits.Count,
+                              text);
+
+            // amount of hits
+            foreach (Match aHit in hits)
+            {
+                GroupCollection groups = aHit.Groups;
+                Console.WriteLine("'{0}' found at {1}",
+                                  groups["word"].Value,
+                                  groups[0].Index
+                                 );
+            }
+            Console.ReadLine();
+        }
+    }
+}
