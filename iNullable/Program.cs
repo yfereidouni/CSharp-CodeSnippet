5795eb80380043a59f9339673cb47ec9cf614320
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-04-27 20:40:32 +0430

    Usage of Nullables.

---
+++ iNullable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace iNullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable int
+            int? num1 = null;
+            int? num2 = 1337;
+
+            double? num3 = new double?();
+            double? num4 = 3.14157;
+
+            bool? boolval = new bool?();
+
+            Console.WriteLine("num1: {0} num2: {1} num3: {2} num4: {3} bool: {4}", num1, num2, num3, num4, boolval);
+
+            Console.WriteLine("----------------------------------------------------");
+
+            bool? isMale = null;
+            if (isMale==true)
+            {
+                Console.WriteLine("Male");
+            }
+            else if (isMale == false)
+            {
+                Console.WriteLine("Female");
+            }
+            else
+            {
+                Console.WriteLine("No gender chosen");
+            }
+        }
+    }
+}
