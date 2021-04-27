c53670a62e1d4b81ddb8b3a198cba2c743999232
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-04-27 21:38:02 +0430

    Nullable ternary operator.

--- iNullable/Program.cs
+++ iNullable/Program.cs
@@ -9,18 +9,13 @@ namespace iNullable
             //Nullable int
             int? num1 = null;
             int? num2 = 1337;
-
             double? num3 = new double?();
             double? num4 = 3.14157;
-
             bool? boolval = new bool?();
-
             Console.WriteLine("num1: {0} num2: {1} num3: {2} num4: {3} bool: {4}", num1, num2, num3, num4, boolval);
-
             Console.WriteLine("----------------------------------------------------");
-
             bool? isMale = null;
-            if (isMale==true)
+            if (isMale == true)
             {
                 Console.WriteLine("Male");
             }
@@ -32,6 +27,11 @@ namespace iNullable
             {
                 Console.WriteLine("No gender chosen");
             }
+            Console.WriteLine("----------------------------------------------------");
+            double? num5 = null;
+            double num6;
+            num6 = num5 ?? 8.4;
+            Console.WriteLine(num6);
         }
     }
 }
