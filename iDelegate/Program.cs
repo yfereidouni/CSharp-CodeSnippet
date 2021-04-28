3913193523aae3f80519b88f14ea0dfaf1f4ea59
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-04-28 18:45:39 +0430

    Lambda Expression was added.

--- iDelegate/Program.cs
+++ iDelegate/Program.cs
@@ -6,6 +6,7 @@ namespace iDelegate
     {
         public delegate void iDelegate(string value);
         public delegate string GetTextDelegate(string name);
+        public delegate double GetResultMultiply(double a, double b);
         static void Main(string[] args)
         {
             iDelegate myTask = SayHello;
@@ -21,6 +22,18 @@ namespace iDelegate
             };
             Console.WriteLine(getTextDelegate("Yasser"));
             //----------------------------------------------------------
+
+            GetResultMultiply getResultMultiply = (a, b) => a * b;
+            GetResultMultiply getResultSum = (a, b) => a + b;
+            GetResultMultiply getResultSub = (a, b) => a - b;
+            DisplayMultiply(getResultMultiply);
+            DisplayMultiply(getResultSum);
+            DisplayMultiply(getResultSub);
+        }
+
+        public static void DisplayMultiply(GetResultMultiply getResultMultiply)
+        {
+            Console.WriteLine("Lambda Expression: {0}",getResultMultiply(3, 4)); 
         }
         public static void SayHello(string value)
         {
