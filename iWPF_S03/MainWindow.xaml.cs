edb39804afdb58df35741e707849bd777aadf12c
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-05-03 21:59:00 +0430

    RichTextBox & TextRange Class

---
+++ iWPF_S03/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iWPF_S03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange tr = new TextRange(rtbDescription.Document.ContentStart, rtbDescription.Document.ContentEnd);
+            MessageBox.Show(tr.Text);
+        }
+    }
+}
