7c91b557b24764f4b317749fae3927a4c536f7f6
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-05-06 19:33:50 +0430

    ListView and Menu

--- iWPF_S04/MainWindow.xaml.cs
+++ iWPF_S04/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace iWPF_S04
     public partial class MainWindow : Window
     {
         public int PersonId { get; set; }
+        List<Entities.Person> PersonList = new List<Entities.Person>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,14 +36,18 @@ namespace iWPF_S04
             //dgvStudents.Items.Add(person1);
 
             //Method 2 : ItemsSource
-            List<Entities.Person> PersonList = new List<Entities.Person>();
+            PersonInstances();
+            dgvStudents.ItemsSource = PersonList;
+        }
+
+        private void PersonInstances()
+        {
             Entities.Person person1 = new Entities.Person() { Id = GetId(), FirstName = "Yasser", LastName = "Fereidouni", Age = 36, BirthDate = new DateTime(1985, 03, 30), Address = "Karaj" };
             Entities.Person person2 = new Entities.Person() { Id = GetId(), FirstName = "Navid", LastName = "Bahrami", Age = 36, BirthDate = new DateTime(1985, 03, 30), Address = "Karaj" };
             Entities.Person person3 = new Entities.Person() { Id = GetId(), FirstName = "Kamran", LastName = "Zarifi", Age = 36, BirthDate = new DateTime(1985, 03, 30), Address = "Karaj" };
             PersonList.Add(person1);
             PersonList.Add(person2);
             PersonList.Add(person3);
-            dgvStudents.ItemsSource = PersonList;
         }
 
         private int GetId()
@@ -53,6 +59,7 @@ namespace iWPF_S04
         private void ButtonStep2_Click(object sender, RoutedEventArgs e)
         {
             tcStages.SelectedIndex = tcStages.SelectedIndex + 1;
+            lvPersons.ItemsSource = PersonList;
         }
 
         private void ButtonStep3_Click(object sender, RoutedEventArgs e)
