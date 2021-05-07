2cebb23f3b064d44e37944f7cf836503baa5c796
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-05-07 10:14:32 +0430

    AnalogClock with WPF was completed.

---
+++ iWPF_S05_AnalogClock/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iWPF_S05_AnalogClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer timer = new Timer(1000);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PersianCalendar pc = new PersianCalendar();
+            DateTime dt = new DateTime(pc.GetYear(DateTime.Now), pc.GetMonth(DateTime.Now), pc.GetDayOfMonth(DateTime.Now));
+            txtbPersianDate.Text = dt.ToString(string.Format("yyyy/MM/dd", dt));
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke((Action)(()=>{
+
+                seconds.Angle = DateTime.Now.Second * 6;
+                minutes.Angle = DateTime.Now.Minute * 6;
+                hours.Angle = DateTime.Now.Hour * 30;
+
+            }));
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
