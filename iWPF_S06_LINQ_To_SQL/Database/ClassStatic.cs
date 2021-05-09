ff54ba4ef1d3257ef401c8b8051e12354810d94d
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-05-09 18:09:25 +0430

    Local and LAN Connection String was added.

--- iWPF_S06_LINQ_To_SQL/Database/ClassStatic.cs
+++ iWPF_S06_LINQ_To_SQL/Database/ClassStatic.cs
@@ -8,7 +8,11 @@ namespace iWPF_S06_LINQ_To_SQL.Database
 {
     public class ClassStatic
     {
-        public static LinqToSQLDataContext dbContext = new LinqToSQLDataContext("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=LinqToSQL;Data Source=.");
+        //Local Connection String:
+        //public static LinqToSQLDataContext dbContext = new LinqToSQLDataContext("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=LinqToSQL;Data Source=.");
+
+        //LAN Connection String:
+        public static LinqToSQLDataContext dbContext = new LinqToSQLDataContext("Password=Develop2011~;Persist Security Info=True;User ID=sa;Initial Catalog=LinqToSQL;Data Source=192.168.1.106");
 
     }
 }
