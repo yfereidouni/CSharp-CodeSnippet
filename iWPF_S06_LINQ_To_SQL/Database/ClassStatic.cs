5c53b976d27b485eee837f589c49f013675a8a90
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-05-09 18:06:23 +0430

    Connection String was changed.

--- iWPF_S06_LINQ_To_SQL/Database/ClassStatic.cs
+++ iWPF_S06_LINQ_To_SQL/Database/ClassStatic.cs
@@ -8,7 +8,7 @@ namespace iWPF_S06_LINQ_To_SQL.Database
 {
     public class ClassStatic
     {
-        public static LinqToSQLDataContext dbContext = new LinqToSQLDataContext("Password=Develop2011~;Persist Security Info=True;User ID=sa;Initial Catalog=LinqToSQL;Data Source=192.168.1.106");
+        public static LinqToSQLDataContext dbContext = new LinqToSQLDataContext("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=LinqToSQL;Data Source=.");
 
     }
 }
