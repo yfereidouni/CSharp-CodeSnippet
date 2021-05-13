1221b0336b46cf16f3edba7f708961e4770da058
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-05-13 11:41:13 +0430

    StringFormats

---
+++ iWPF_S11_StringFormats/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iWPF_S11_StringFormats
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new myStringFormat();
+            //decimal ff = 20000000;
+            //txtPrice.Text = ff.ToString("N0");
+        }
+    }
+
+    public class myStringFormat
+    {
+        public decimal money { get; set; }
+    }
+}
