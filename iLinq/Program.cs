e52985ab177dca82d469a31135a1c6ca692e8c35
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-05-16 20:47:50 +0430

    Linq gentle introduction

---
+++ iLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace iLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            OddNumbers(numbers);
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Numbers : ");
+
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+            Console.WriteLine(oddNumbers);
+
+            foreach (var item in oddNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
