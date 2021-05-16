7b60891f84e4ca7b81b67c94f47514c6780c01e6
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-05-16 19:45:51 +0430

    ZooManager app was finished.

--- iWPF_ZooManager/MainWindow.xaml.cs
+++ iWPF_ZooManager/MainWindow.xaml.cs
@@ -28,10 +28,14 @@ namespace iWPF_ZooManager
         public MainWindow()
         {
             InitializeComponent();
-
+#if DEBUG
+            var connectionString = ConfigurationManager.ConnectionStrings["iWPF_ZooManager.Properties.Settings.WWDBConnectionString_Local"].ConnectionString;
+#else
             var connectionString = ConfigurationManager.ConnectionStrings["iWPF_ZooManager.Properties.Settings.WWDBConnectionString"].ConnectionString;
-            sqlConnection = new SqlConnection(connectionString);
             //sqlConnection = new SqlConnection("Data Source=192.168.1.106;Initial Catalog=WWDB;Persist Security Info=True;User ID=sa;Password=Develop2011~");
+#endif
+
+            sqlConnection = new SqlConnection(connectionString);
 
             ShowZoos();
             ShowAllAnimals();
@@ -53,7 +57,7 @@ namespace iWPF_ZooManager
                     listZoos.ItemsSource = zooTable.DefaultView;
                 }
                 //Automatically select first item of zoo list -------------------
-                if (listZoos.Items.Count >= 0) listZoos.SelectedIndex = 0;
+                //if (listZoos.Items.Count >= 0) listZoos.SelectedIndex = 0;
                 //---------------------------------------------------------------
             }
             catch (Exception e)
@@ -103,7 +107,7 @@ namespace iWPF_ZooManager
                     sqlDataAdapter.Fill(animalTable);
 
                     listAssociatedAnimals.DisplayMemberPath = "Name";
-                    listAssociatedAnimals.SelectedValuePath = "Id";
+                    listAssociatedAnimals.SelectedValuePath = "Id1";
                     listAssociatedAnimals.ItemsSource = animalTable.DefaultView;
                 }
             }
@@ -117,6 +121,7 @@ namespace iWPF_ZooManager
         private void listZoos_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ShowAssociatedAnimals();
+            ShowSelectedZooInTextBox();
         }
 
         private void DeleteZoo_Click(object sender, RoutedEventArgs e)
@@ -138,8 +143,214 @@ namespace iWPF_ZooManager
                 sqlConnection.Close();
                 ShowZoos();
             }
-           
-            
         }
+
+        private void AddZoo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO Zoo values (@Location)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Location", txtInputText.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowZoos();
+            }
+        }
+
+        private void AddAnimalToZoo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO ZooAnimal values (@ZooID,@AnimalId)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+                sqlCommand.Parameters.AddWithValue("@AnimalId", listAllAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                //ShowZoos();
+                ShowAssociatedAnimals();
+            }
+        }
+
+        private void AddAnimalZoo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO Animal values (@Name)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Name", txtInputText.Text);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAllAnimals();
+            }
+        }
+
+        private void DeleteAnimalZoo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "DELETE FROM Animal WHERE Id = @Id";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Id", listAllAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAllAnimals();
+            }
+        }
+
+        private void DeleteAnimalFromZoo_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(listAssociatedAnimals.SelectedValue.ToString());
+            try
+            {
+                string query = "DELETE FROM ZooAnimal WHERE Id = @Id";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Id", listAssociatedAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAssociatedAnimals();
+            }
+        }
+
+        private void ShowSelectedZooInTextBox()
+        {
+            try
+            {
+                string query = "SELECT Location FROM Zoo Where Id = @ZooID";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@ZooId", listZoos.SelectedValue);
+
+                    DataTable zooDataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooDataTable);
+
+                    txtInputText.Text = zooDataTable.Rows[0]["Location"].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.Message.ToString());
+            }
+        }
+
+        private void listAllAnimals_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowSelectedallAnimalsInTextBox();
+        }
+
+        private void ShowSelectedallAnimalsInTextBox()
+        {
+            try
+            {
+                string query = "SELECT Name FROM Animal Where Id = @Id";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue("@Id", listAllAnimals.SelectedValue);
+
+                    DataTable zooDataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooDataTable);
+
+                    txtInputText.Text = zooDataTable.Rows[0]["Name"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void UpdateZoo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "UPDATE Zoo SET Location = @Location WHERE Id = @Id";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Location", txtInputText.Text);
+                sqlCommand.Parameters.AddWithValue("@Id", listZoos.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowZoos();
+            }
+        }
+
+        private void UpdateAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "UPDATE Animal SET Name = @Name WHERE Id = @Id";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.Parameters.AddWithValue("@Name", txtInputText.Text);
+                sqlCommand.Parameters.AddWithValue("@Id", listAllAnimals.SelectedValue);
+                sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAllAnimals();
+            }
+        }
+
     }
 }
--- iWPF_ZooManager/Properties/Settings.Designer.cs
+++ iWPF_ZooManager/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace iWPF_ZooManager.Properties {
                 return ((string)(this["WWDBConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=WWDB;Persist Security Info=True;User ID=sa;Password" +
+            "=Develop2011~")]
+        public string WWDBConnectionString_Local {
+            get {
+                return ((string)(this["WWDBConnectionString_Local"]));
+            }
+        }
     }
 }
