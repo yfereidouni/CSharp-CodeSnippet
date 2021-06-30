cc6bea3fd4db7c9459417b2c36f8fb2d7b93f7ef
Yasser Fereidouni <yasser.fereidouni@me.com>
2021-06-30 21:14:48 +0430

    LinkedList

---
+++ iLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words =
+            { "the", "fox", "jumps", "over", "the", "dog" };
+
+            LinkedList<string> sentence = new LinkedList<string>(words);
+
+            LinkedListNode<string> mark1 = sentence.Find("over");
+
+            sentence.AddBefore(mark1, "Yasser");
+
+            foreach (string word in sentence)
+            {
+                Console.Write(word + " ");
+            }
+        }
+    }
+}
