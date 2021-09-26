d808cc1da1254e4bdbdd1a85a4b93350debbc892
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-09-26 15:24:56 +0330

    Done

--- iWPF_MVVM/App.xaml.cs
+++ iWPF_MVVM/App.xaml.cs
@@ -9,6 +9,7 @@ namespace iWPF_MVVM
     /// </summary>
     public partial class App : Application
     {
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
