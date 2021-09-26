4776a4457b56f053d5159659a2e52e54edb523e9
Yasser Fereidouni <Yasser.Fereidouni@me.com>
2021-09-26 14:58:13 +0330

    Add WPF application with MVVM model

---
+++ iWPF_MVVM/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace iWPF_MVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            iWPF_MVVM.MainWindow window = new MainWindow();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+
+    
+}
