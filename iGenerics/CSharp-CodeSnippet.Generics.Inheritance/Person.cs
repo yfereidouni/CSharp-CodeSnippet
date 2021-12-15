599b424ee768916c334dc0e12bde9430e75b7965
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-15 18:30:41 +0330

    Foldering

---
+++ iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_CodeSnippet.Generics.Inheritance
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() => $"Person: {Id} | {FirstName} | {LastName}";
+    }
+
+    public class Student : Person
+    {
+        public string StudentCode { get; set; }
+        public override string ToString() => $"Student: {Id} | {FirstName} | {LastName} | {StudentCode}";
+
+    }
+
+    public class Teacher : Person
+    {
+        public Teacher()
+        {
+            Id = 1;
+            FirstName = "Soroush";
+            LastName = "Sarabi";
+            TeacherCode = "T-1020";
+        }
+        public string TeacherCode { get; set; }
+        public override string ToString() => $"Teacher: {Id} | {FirstName} | {LastName} | {TeacherCode}";
+    }
+
+    public class Printer<T> where T : Person
+    {
+        public string PrintPersonnel(T person) => person.ToString();
+    }
+    
+    /// <summary>
+    /// Inheritance in Generics : Child is non-Generic and parent is Generic
+    /// </summary>
+    public class PrintNonGeneric : Printer<Person> 
+    {
+        public string PrintNonGenericPersonnel(Person person) => person.ToString();
+    }
+
+   
+
+}
--- iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Program.cs
+++ iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using CSharp_CodeSnippet.Generics.Inheritance;
+
+Person p01 = new Person { Id = 1, FirstName = "Yasser", LastName = "Fereidouni" };
+Student p02 = new Student { Id = 1, FirstName = "Aman", LastName = "Toumaj", StudentCode = "ST-1010" };
+Teacher p03 = new Teacher { Id = 1, FirstName = "Soroush", LastName = "Sarabi", TeacherCode = "T-1020" };
+
+
+Printer<Person> printer = new Printer<Person>();
+
+Console.WriteLine(printer.PrintPersonnel(p01));
+Console.WriteLine(printer.PrintPersonnel(p02));
+Console.WriteLine(printer.PrintPersonnel(p03));
