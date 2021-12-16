81147b5bffd644f5881c37a33b4bf4da2a81dd16
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-16 10:28:04 +0330

    Foldering

--- iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Person.cs
+++ iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Person.cs
@@ -34,19 +34,50 @@ namespace CSharp_CodeSnippet.Generics.Inheritance
         public override string ToString() => $"Teacher: {Id} | {FirstName} | {LastName} | {TeacherCode}";
     }
 
-    public class Printer<T> where T : Person
+    public class Printer<T1> where T1 : Person
     {
-        public string PrintPersonnel(T person) => person.ToString();
-    }
-    
-    /// <summary>
-    /// Inheritance in Generics : Child is non-Generic and parent is Generic
-    /// </summary>
-    public class PrintNonGeneric : Printer<Person> 
-    {
-        public string PrintNonGenericPersonnel(Person person) => person.ToString();
+        public virtual string Print(T1 person) => person.ToString();
     }
 
-   
+    public class Printer<T1, T2> where T1 : Person where T2 : Person
+    {
+        public virtual string Print(T1 p1, T2 p2) => $"{p1.ToString()}\r\n{p2.ToString()}";
+    }
 
+    public class Printer<T1, T2, T3> 
+        where T1 : Person 
+        where T2 : Person 
+        where T3 : Person
+    {
+        public virtual string Print(T1 p1, T2 p2) => $"{p1.ToString()}\r\n{p2.ToString()}";
+    }
+
+    /// Inheritanc in Generics :
+
+    /// 1. Child is non-Generic and parent is Generic (Type of Generic must be specified)
+    public class PersonPrinter : Printer<Person>
+    {
+    }
+
+    /// 2. Child is Generic and parent is Generic (child and parent are Generic both)
+    public class PersonPrinter<T> : Printer<T> 
+        where T : Person
+    {
+    }
+
+    /// 3. Child is Generic and parent has one define type and one Generic
+    public class PersonPrinter<T1,T2> : Printer<Teacher,T1,T2> 
+        where T1 : Person
+        where T2: Person
+    {
+    }
+
+    ///Example of Input and output Generics
+    public class TestClass<TInput, TOutput>
+    {
+        public TOutput Test(TInput input)
+        {
+            return default;
+        }
+    }
 }
--- iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Program.cs
+++ iGenerics/CSharp-CodeSnippet.Generics.Inheritance/Program.cs
@@ -3,12 +3,19 @@
 using CSharp_CodeSnippet.Generics.Inheritance;
 
 Person p01 = new Person { Id = 1, FirstName = "Yasser", LastName = "Fereidouni" };
-Student p02 = new Student { Id = 1, FirstName = "Aman", LastName = "Toumaj", StudentCode = "ST-1010" };
-Teacher p03 = new Teacher { Id = 1, FirstName = "Soroush", LastName = "Sarabi", TeacherCode = "T-1020" };
+Student s01 = new Student { Id = 1, FirstName = "Aman", LastName = "Toumaj", StudentCode = "ST-1010" };
+Teacher t01 = new Teacher { Id = 1, FirstName = "Soroush", LastName = "Sarabi", TeacherCode = "T-1020" };
 
-
+///------------------------------------------------------------------------------------
+Console.WriteLine("".PadRight(50,'-'));
+Console.WriteLine("Genricts with 2 parameters:".ToUpper());
 Printer<Person> printer = new Printer<Person>();
+Console.WriteLine(printer.Print(p01));
+Console.WriteLine(printer.Print(s01));
+Console.WriteLine(printer.Print(t01));
 
-Console.WriteLine(printer.PrintPersonnel(p01));
-Console.WriteLine(printer.PrintPersonnel(p02));
-Console.WriteLine(printer.PrintPersonnel(p03));
+///------------------------------------------------------------------------------------
+Console.WriteLine("".PadRight(50,'-'));
+Console.WriteLine("Genricts with 2 parameters:".ToUpper());
+Printer<Student, Teacher> printer1 = new Printer<Student, Teacher>();
+Console.WriteLine(printer1.Print(s01, t01));
