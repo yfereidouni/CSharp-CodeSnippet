ad71d6f31cee33bd4253e68432e0cb2300b26bc3
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-16 17:37:20 +0330

    Func & Action & Delegates

---
+++ iSorts/iSorts.BubbleSort/BubbleSort.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSorts.BubbleSort
+{
+    //public delegate bool SortDirection(int input01, int input02);
+
+    /// Delegates without Output
+    public delegate void Delegate01();
+    public delegate void Delegate01<T1>(T1 t1);
+    public delegate void Delegate01<T1, T2>(T1 t1, T2 t2);
+    public delegate void Delegate01<T1, T2, T3>(T1 t1, T2 t2, T3 t3k);
+
+    /// Delegates with output 
+    public delegate Tout Delegate02<Tout>();
+    public delegate Tout Delegate02<T1, Tout>(T1 t1);
+    public delegate Tout Delegate02<T1, T2, Tout>(T1 t1, T2 t2);
+    public delegate Tout Delegate02<T1, T2, T3, Tout>(T1 t1, T2 t2, T3 t3k);
+
+    public class BubbleSort
+    {
+        public Func<int, int, bool> SortOption;
+
+        public int[] bubblesort(int[] inputs)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                for (int j = i; j < inputs.Length; j++)
+                {
+                    int temp = 0;
+                    if (inputs[i] < inputs[j])
+                    {
+                        temp = inputs[i];
+                        inputs[i] = inputs[j];
+                        inputs[j] = temp;
+                    }
+                }
+            }
+            return inputs;
+        }
+
+        public int[] bubblesort(int[] inputs, bool IsAsc)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                for (int j = i; j < inputs.Length; j++)
+                {
+                    int temp = 0;
+                    if ((IsAsc && inputs[i] < inputs[j]) || (!IsAsc && inputs[i] > inputs[j]))
+                    {
+                        temp = inputs[i];
+                        inputs[i] = inputs[j];
+                        inputs[j] = temp;
+                    }
+                }
+            }
+            return inputs;
+        }
+
+        #region public int[] bubblesort(int[] inputs, SortDirection sortDirection)
+        //public int[] bubblesort(int[] inputs, SortDirection sortDirection)
+        //{
+        //    for (int i = 0; i < inputs.Length; i++)
+        //    {
+        //        for (int j = i; j < inputs.Length; j++)
+        //        {
+        //            int temp = 0;
+        //            if (sortDirection.Invoke(inputs[i], inputs[j]))
+        //            {
+        //                temp = inputs[i];
+        //                inputs[i] = inputs[j];
+        //                inputs[j] = temp;
+        //            }
+        //        }
+        //    }
+        //    return inputs;
+        //}
+        #endregion
+
+        #region public int[] bubblesort(int[] inputs, Delegate02<int, int, bool> delegate02)
+        //public int[] bubblesort(int[] inputs, Delegate02<int, int, bool> delegate02)
+        //{
+        //    for (int i = 0; i < inputs.Length; i++)
+        //    {
+        //        for (int j = i; j < inputs.Length; j++)
+        //        {
+        //            int temp = 0;
+        //            if (delegate02.Invoke(inputs[i], inputs[j]))
+        //            {
+        //                temp = inputs[i];
+        //                inputs[i] = inputs[j];
+        //                inputs[j] = temp;
+        //            }
+        //        }
+        //    }
+        //    return inputs;
+        //}
+        #endregion
+
+        public int[] bubblesort(int[] inputs, Func<int, int, bool> delegate02)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                for (int j = i; j < inputs.Length; j++)
+                {
+                    int temp = 0;
+                    if (delegate02.Invoke(inputs[i], inputs[j]))
+                    {
+                        temp = inputs[i];
+                        inputs[i] = inputs[j];
+                        inputs[j] = temp;
+                    }
+                }
+            }
+            return inputs;
+        }
+
+        public bool Asc(int input01, int input02)
+            => input01 > input02;
+
+        public bool Desc(int input01, int input02)
+            => input01 < input02;
+
+        public bool newSort(int input01, int input02)
+            => input01 > input02 && DateTime.Now.Hour > 12;
+
+        public void PrintResult(int[] inputs)
+        {
+            foreach (var item in inputs)
+            {
+                Console.Write(item + "".PadRight(2, ' '));
+            }
+            Console.WriteLine("\r\n".PadRight(50, '-'));
+        }
+
+    }
+
+
+}
---
+++ iSorts/iSorts.BubbleSort/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using iSorts.BubbleSort;
+
+Console.WriteLine("Bubble Sort\r\n".PadRight(61, '-'));
+
+int[] inputs = new int[] { 10, 5, 4, 3, 2, 7, 1, 6 };
+
+//GeneralBubbleSort();
+
+//GeneralBubbleSortWithAscendingOption();
+
+//BubbleSortWithDelegate();
+
+//BubbleSortWithEndlessDelegate();
+
+BubbleSortWithFuncAndAction();
+
+//------------------------------------- Implementations ------------------------------------------
+
+void GeneralBubbleSort()
+{
+    ///General Bubble Sort
+    BubbleSort bubbleSortGeneral = new();
+    var resultGeneral = bubbleSortGeneral.bubblesort(inputs);
+    bubbleSortGeneral.PrintResult(resultGeneral);
+}
+
+void GeneralBubbleSortWithAscendingOption()
+{
+    ///General Bubble Sort with Ascending option
+    BubbleSort bubbleSortAsc = new();
+    var resultAsc = bubbleSortAsc.bubblesort(inputs, true);
+    bubbleSortAsc.PrintResult(resultAsc);
+    //-------
+    BubbleSort bubbleSortDesc = new();
+    var resultDesc = bubbleSortDesc.bubblesort(inputs, false);
+    bubbleSortDesc.PrintResult(resultDesc);
+}
+
+void BubbleSortWithDelegate()
+{
+    BubbleSort bubbleSortDelegate = new();
+    var resultAsc = bubbleSortDelegate.bubblesort(inputs, bubbleSortDelegate.Asc);
+    bubbleSortDelegate.PrintResult(resultAsc);
+    
+    var resultDesc = bubbleSortDelegate.bubblesort(inputs, bubbleSortDelegate.Desc);
+    bubbleSortDelegate.PrintResult(resultDesc);
+    
+    var resultNewSort = bubbleSortDelegate.bubblesort(inputs, bubbleSortDelegate.newSort);
+    bubbleSortDelegate.PrintResult(resultNewSort);
+}
+
+void BubbleSortWithEndlessDelegate()
+{
+    //BubbleSort bubbleSortDelegate = new();
+    //Delegate02<int, int, bool> delegate02;
+
+    //delegate02 = bubbleSortDelegate.Asc;
+    //var resultAsc = bubbleSortDelegate.bubblesort(inputs, delegate02);
+    //bubbleSortDelegate.PrintResult(resultAsc);
+
+    //delegate02 = bubbleSortDelegate.Desc;
+    //var resultDesc = bubbleSortDelegate.bubblesort(inputs, delegate02);
+    //bubbleSortDelegate.PrintResult(resultDesc);
+
+    //delegate02 = bubbleSortDelegate.newSort;
+    //var resultNewSort = bubbleSortDelegate.bubblesort(inputs, delegate02);
+    //bubbleSortDelegate.PrintResult(resultNewSort);
+
+}
+
+void BubbleSortWithFuncAndAction()
+{
+    BubbleSort bubbleSortFuncAndAction = new();
+    bubbleSortFuncAndAction.SortOption = bubbleSortFuncAndAction.Desc;
+    var result = bubbleSortFuncAndAction.bubblesort(inputs, bubbleSortFuncAndAction.SortOption);
+    bubbleSortFuncAndAction.PrintResult(result);
+}
+
+
