f889d8be36b092bdfec80d0e6db088e0d0f425cd
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-16 10:35:55 +0330

    Renaming

--- iWPF/iWPF_S06_HandyControls/Properties/Settings.Designer.cs
+++ iWPF/iWPF_S06_HandyControls/Properties/Settings.Designer.cs
@@ -8,11 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace iWPF_S06_LINQ_To_SQL.Properties {
+namespace iWPF.S06_HandyControls.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
