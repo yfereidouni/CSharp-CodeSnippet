7fc19b68cdfbede9d20f3d0a2175b58b73b27db3
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-17 12:21:59 +0330

    Where Clause in LINQ

--- iLINQ/iLINQ.ReadFromFile/Program.cs
+++ iLINQ/iLINQ.ReadFromFile/Program.cs
@@ -19,9 +19,8 @@ foreach (var item in lines)
 }
 
 foreach (var item in people)
-{
     Console.WriteLine(item.ToString());
-}
+
 // With LINQ ----------------------------------------------------------------------------------
 Console.WriteLine("\r\nLINQ : ");
 List<Person> Students = System.IO.File.ReadAllLines("People.txt").Select(c =>
@@ -36,9 +35,26 @@ List<Person> Students = System.IO.File.ReadAllLines("People.txt").Select(c =>
     };
 
 }).ToList();
-
 foreach (var item in Students)
-{
     Console.WriteLine(item.ToString());
-}
 
+// Another LINQ Query 1 ------------------------------------------------------------------------
+Console.WriteLine("\r\nAnother LINQ Query 1 : ");
+
+var resultDateTime1 = Students.Select(c => c.Birthdate);//.ToList();
+// Equal with : 
+//var resultDateTime2 = Students.Select(c => { return c.Birthdate; });//.ToList();
+
+foreach (var item in resultDateTime1)
+    Console.WriteLine(item);
+
+
+// Another LINQ Query 2 ------------------------------------------------------------------------
+Console.WriteLine("\r\nAnother LINQ Query 2 : ");
+
+var result2 = Students.Where(c => c.Birthdate < new DateTime(1973, 01, 01));
+// Equal with:
+// var result3 = Students.Where(c => { return c.Birthdate < new DateTime(1973, 01, 01); });
+
+foreach (var item in result2)
+    Console.WriteLine(item);
