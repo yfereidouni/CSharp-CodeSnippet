5d84d610150f51efb64b240accc4a5946d4afb1b
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-17 10:23:02 +0330

    LINQ

---
+++ iLINQ/iLINQ.ReadFromFile/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iLINQ.ReadFromFile
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public override string ToString()
+        {
+            //return $"{Id} |\t{FirstName} |\t{LastName} |\t{Birthdate}";
+            return $"{Id}".PadRight(8, ' ')+
+                   $" | {FirstName}".PadRight(15, ' ')+
+                   $" | {LastName}".PadRight(15, ' ')+
+                   $" | {Birthdate}".PadRight(20, ' ');
+        }
+    }
+
+}
---
+++ iLINQ/iLINQ.ReadFromFile/Program.cs
@@ -0,0 +1,44 @@
+﻿using iLINQ.ReadFromFile;
+using System.Linq;
+
+// Without LINQ ------------------------------------------------------------------------------
+Console.WriteLine("\r\nWithout LINQ : ");
+var lines = System.IO.File.ReadAllLines("People.txt");
+List<Person> people = new List<Person>();
+foreach (var item in lines)
+{
+    var result = item.Split(",");
+    Person person = new Person
+    {
+        Id = int.Parse(result[0]),
+        FirstName = result[1],
+        LastName = result[2],
+        Birthdate = DateTime.Parse(result[3])
+    };
+    people.Add(person);
+}
+
+foreach (var item in people)
+{
+    Console.WriteLine(item.ToString());
+}
+// With LINQ ----------------------------------------------------------------------------------
+Console.WriteLine("\r\nLINQ : ");
+List<Person> Students = System.IO.File.ReadAllLines("People.txt").Select(c =>
+{
+    var data = c.Split(",");
+    return new Person
+    {
+        Id = int.Parse(data[0]),
+        FirstName = data[1],
+        LastName = data[2],
+        Birthdate = DateTime.Parse(data[3])
+    };
+
+}).ToList();
+
+foreach (var item in Students)
+{
+    Console.WriteLine(item.ToString());
+}
+
