825db18d0e646eb53c5462fa60cc08991275e9b5
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2021-12-18 22:07:49 +0330

    bugfix

---
+++ iLINQ/iLINQ.ReadFromFile/MyLinq.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iLINQ.ReadFromFile
+{
+    public static class MyLinq
+    {
+        public static List<Tinput> Where2<Tinput>(this IEnumerable<Tinput> inputList, Func<Tinput, bool> func)
+        {
+            List<Tinput> result = new List<Tinput>();
+            foreach (var item in inputList)
+            {
+                if (func.Invoke(item))
+                {
+                    result.Add(item); 
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<Tinput> Where3<Tinput>(this IEnumerable<Tinput> inputList, Func<Tinput, bool> func)
+        {
+            foreach (var item in inputList)
+            {
+                if (func.Invoke(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
