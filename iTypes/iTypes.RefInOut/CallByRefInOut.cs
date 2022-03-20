bda8a9a585d9517f622082cdb7597fbc210c030d
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-03-20 21:26:06 +0330

    Ref-Out-In

---
+++ iTypes/iTypes.RefInOut/CallByRefInOut.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTypes.RefInOut
+{
+    public class CallByRefInOut
+    {
+        public void CallByRef()
+        {
+            int myValue = 1;
+            Console.WriteLine("Call By Ref:");
+            Console.WriteLine(myValue);
+            CalleeByRef(ref myValue);
+            Console.WriteLine(myValue);
+            Console.WriteLine("".PadRight(50,'-'));
+        }
+        public void CalleeByRef(ref int input)
+        {
+            input = 3; // we can change the value but it is not required
+        }
+
+        public void CallByOut()
+        {
+            Console.WriteLine("Call By Out:");
+            CalleeByOut(out int myValue);
+            Console.WriteLine(myValue);
+            Console.WriteLine("".PadRight(50, '-'));
+
+        }
+        public void CalleeByOut(out int input)
+        {
+            input = 3;
+        }
+
+        public void CallByIn()
+        {
+            int myValue = 1;
+            Console.WriteLine("Call By In:");
+            Console.WriteLine(myValue);
+            CalleeByIn(in myValue);
+            Console.WriteLine(myValue);
+            Console.WriteLine("".PadRight(50, '-'));
+
+        }
+        public void CalleeByIn(in int input)
+        {
+            //input = 3; //Error: we cannot change it because it is read-only
+        }
+
+    }
+}
---
+++ iTypes/iTypes.RefInOut/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using iTypes.RefInOut;
+
+CallByRefInOut callByRefInOut = new CallByRefInOut();
+
+callByRefInOut.CallByRef();
+
+callByRefInOut.CallByOut();
+
+callByRefInOut.CallByIn();
