dff26349042a89de635e5a623b98e1f3216cb5eb
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-03-29 23:41:39 +0430

    SortedDictionary

--- iCollections/iCollections.Dictionaries/Program.cs
+++ iCollections/iCollections.Dictionaries/Program.cs
@@ -3,6 +3,8 @@
 
 using iCollections.Dictionaries;
 
+Console.WriteLine("------------------------------------");
+Console.WriteLine("Dictionary:\r\n");
 DictionarySample dictionarySample = new DictionarySample();
 
 dictionarySample.Add("1st", "Yasser");
@@ -15,3 +17,21 @@ dictionarySample.Remove("2nd");
 Console.WriteLine("\r\nItem in Index 2nd removed from Dictionary\r\n");
 
 dictionarySample.PrintAll();
+Console.WriteLine();
+
+//---------------------------------------------
+Console.WriteLine("------------------------------------");
+Console.WriteLine("SortedDictionary:\r\n");
+SortedDictionarySample sortedDictionarySample = new SortedDictionarySample();
+
+sortedDictionarySample.Add("1st", "Yasser");
+sortedDictionarySample.Add("2nd", "Shervin");
+sortedDictionarySample.Add("3rd", "Soroush");
+
+sortedDictionarySample.PrintAll();
+
+sortedDictionarySample.Remove("2nd");
+Console.WriteLine("\r\nItem in Index 2nd removed from Dictionary\r\n");
+
+sortedDictionarySample.PrintAll();
+
---
+++ iCollections/iCollections.Dictionaries/SortedDictionarySample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCollections.Dictionaries;
+
+public class SortedDictionarySample
+{
+    SortedDictionary<string, string> dictionary = new SortedDictionary<string, string>();
+
+    public void Add(string key, string value)
+    {
+        if (!dictionary.ContainsKey(key))
+            dictionary[key] = value;
+    }
+
+    public void Remove(string key)
+    {
+        dictionary.Remove(key);
+    }
+
+    public void PrintAll()
+    {
+        foreach (var item in dictionary)
+        {
+            Console.WriteLine($"{item.Key} | {item.Value}");
+        }
+    }
+}
