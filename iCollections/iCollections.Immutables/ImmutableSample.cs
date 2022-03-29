9ce4949002cb48d067b59964c6562f47596eb6c0
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-03-30 00:10:01 +0430

    ImmutableCollections

---
+++ iCollections/iCollections.Immutables/ImmutableSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCollections.Immutables;
+
+public class ImmutableSample
+{
+    public void Test()
+    {
+        ImmutableList<string> immutableList = ImmutableList.Create<string>();
+
+        Console.WriteLine(immutableList.Count);
+
+        var result = immutableList.Add("hi").Add("hi").Add("hi");
+
+        Console.WriteLine(immutableList.Count);
+        Console.WriteLine(result.Count);
+    }
+}
---
+++ iCollections/iCollections.Immutables/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using iCollections.Immutables;
+
+ImmutableSample immutableSample = new ImmutableSample();
+
+immutableSample.Test();
+
