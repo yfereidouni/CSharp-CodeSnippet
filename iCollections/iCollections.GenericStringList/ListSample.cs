4bf5f750e3395a9f368533ac2a56ca01c64db8fa
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-03-29 10:29:51 +0430

    Generic Lists

---
+++ iCollections/iCollections.GenericStringList/ListSample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCollections.GenericStringList
+{
+    public class ListSample
+    {
+        List<string> list = new List<string>();
+
+        public void PrintCapacity()
+        {
+            Console.WriteLine($"Capacity: {list.Capacity} | Count: {list.Count} \r\n");
+        }
+
+        public void AddMember(string input)
+        {
+            list.Add(input);
+            Console.WriteLine($"\tMessage => {input} was added.\r\n");
+        }
+
+        public void Ensure()
+        {
+            list.EnsureCapacity(20);
+            Console.WriteLine("\tMessage => List was EnsureCapacity(20)!\r\n");
+
+        }
+
+        public void Trim()
+        {
+            list.TrimExcess();
+            Console.WriteLine("\tMessage => List was TrimExcess!\r\n");
+        }
+    }
+}
---
+++ iCollections/iCollections.GenericStringList/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using iCollections.GenericStringList;
+
+ListSample ls = new ListSample();
+
+ls.PrintCapacity();
+ls.AddMember("Yasser");
+ls.PrintCapacity();
+//ls.Ensure();
+ls.Trim();
+ls.PrintCapacity();
+
+ls.AddMember("Shervin");
+ls.AddMember("Soroush");
+//ls.AddMember("Majid");
+//ls.AddMember("Kazem");
+
+ls.PrintCapacity();
+ls.Trim();
+ls.PrintCapacity();
+
+ls.Ensure();
+ls.PrintCapacity();
