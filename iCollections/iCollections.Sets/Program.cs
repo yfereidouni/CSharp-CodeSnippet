b9d4a3c8b580af8f91786e5058484bafedf31138
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-03-29 23:52:44 +0430

    Set and HashSet

---
+++ iCollections/iCollections.Sets/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using iCollections.Sets;
+
+
+SetSample setSample = new SetSample();
+
+setSample.Add("01");
+setSample.Add("02");
+setSample.Add("03");
+
+setSample.PrintAll();
+Console.WriteLine("--------------------");
+setSample.Add("02");
+setSample.Add("03");
+
+setSample.PrintAll();
+ 
---
+++ iCollections/iCollections.Sets/SetSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCollections.Sets
+{
+    public class SetSample
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        public void Add(string input)
+        {
+            set.Add(input);
+        }
+
+        public void PrintAll()
+        {
+            foreach (var item in set)
+                Console.WriteLine(item);
+        }
+        
+        public void Operators()
+        {
+            HashSet<string> newSet = new HashSet<string>();
+            newSet.Add("01");
+            newSet.ExceptWith(set);
+            newSet.UnionWith(set);
+            newSet.IntersectWith(set);
+            newSet.IsProperSubsetOf(set);
+            newSet.IsSupersetOf(set);
+        }
+    }
+}
