11c1f31349b48ebbad947015a9d656db3c2c3757
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-04-01 09:16:44 +0430

    iDateTime

---
+++ iTypes/iTypes.DateTimes/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using iTypes.DateTimes;
+
+iDateTime dateTime = new iDateTime();
+
+Console.WriteLine($"My birthday is: {dateTime.MyBirthDate}");
+Console.WriteLine($"Now: {dateTime.Now}");
+Console.WriteLine($"Today: {dateTime.Today}");
+Console.WriteLine($"Tomorrow: {dateTime.Tomorrow}");
+Console.WriteLine($"First Day of Week: {dateTime.DayOfWeek}");
+
+Console.WriteLine("----------------------------------------------");
+
+dateTime.DateTimeDetails();
---
+++ iTypes/iTypes.DateTimes/iDateTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTypes.DateTimes
+{
+    public class iDateTime
+    {
+        public DateTime MyBirthDate { get; set; } = new DateTime(1985, 03, 30);
+
+        public DateTime Now { get; set; } = DateTime.Now;
+        public DateTime Today { get; set; } = DateTime.Today;
+        public DayOfWeek DayOfWeek { get; set; } = DateTime.Today.DayOfWeek;
+        public DateTime Tomorrow { get; set; } = DateTime.Today.AddDays(1);
+
+        public void DateTimeDetails()
+        {
+            DateTime dateTiem;
+            if (DateTime.TryParse(this.Today.ToString(), out dateTiem))
+            {
+                Console.WriteLine(dateTiem);
+                TimeSpan daysPassed = DateTime.Now.Subtract(dateTiem);
+                Console.WriteLine("Day passed: {0}", daysPassed);
+            }
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(DateTime.Now.Hour + " : " + DateTime.Now.Minute + " : " + DateTime.Now.Second);
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
