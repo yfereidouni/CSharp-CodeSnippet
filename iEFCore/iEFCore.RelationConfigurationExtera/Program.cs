384b6f69b18f02b2e650831c896e7bfd49b7f578
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-04-23 15:56:10 +0430

    Reading OwnedType data

--- iEFCore/iEFCore.RelationConfigurationExtera/Program.cs
+++ iEFCore/iEFCore.RelationConfigurationExtera/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using iEFCore.RelationConfigurationExtera.OwnedTypeSample;
+
+OwnedTypeDbContext ctx = new OwnedTypeDbContext(); 
+
+var person = ctx.People.FirstOrDefault(); // Without Include you can have all related data to Person
+
+Console.ReadKey();
