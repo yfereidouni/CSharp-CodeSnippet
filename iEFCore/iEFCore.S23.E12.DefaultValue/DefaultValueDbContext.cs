be19f9a00100e441fccfae5558091ad1e9fcff46
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-04-26 14:58:05 +0430

    DefaultValue

---
+++ iEFCore/iEFCore.S23.E12.DefaultValue/DefaultValueDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace iEFCore.S23.E12.DefaultValue;
+
+public class DefaultValueDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Initial Catalog=S23.E12.DefaultValue_DB;User Id=dbuser; Password=1qaz!QAZ;");
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Default Value
+        modelBuilder.Entity<Person>()
+                    .Property(c => c.RegisterDate)
+                    .HasDefaultValueSql("GetDate()");
+
+        modelBuilder.Entity<Person>()
+                    .Property(c => c.Age)
+                    .HasDefaultValue(100);
+
+    }
+}
---
+++ iEFCore/iEFCore.S23.E12.DefaultValue/Program.cs
@@ -0,0 +1,12 @@
+﻿using iEFCore.S23.E12.DefaultValue;
+
+var ctx = new DefaultValueDbContext();
+
+var person = new Person
+{
+    FirstName = "Yasser",
+    LastName = "Fereidouni"
+};
+ctx.People.Add(person);
+ctx.SaveChanges();
+
