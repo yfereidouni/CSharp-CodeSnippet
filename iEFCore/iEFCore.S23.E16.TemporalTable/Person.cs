a68deaf4c168f8fdb15b9b4ec3712973c49ce6e8
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-04-26 11:13:53 +0430

    TemporalTable

---
+++ iEFCore/iEFCore.S23.E16.TemporalTable/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iEFCore.S23.E16.TemporalTable;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
---
+++ iEFCore/iEFCore.S23.E16.TemporalTable/TemporalTableDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace iEFCore.S23.E16.TemporalTable;
+
+public class TemporalTableDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Initial Catalog=S23.E15.RowVersion_DB;User Id=dbuser; Password=1qaz!QAZ;");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().Property(c=>c.FirstName).IsConcurrencyToken();
+    }
+}
