13239ee158039277f1c35d54e707192ac9af9ee2
Yasser Fereidouni <yasser.fereidouni@hotmail.com>
2022-05-17 16:15:00 +0430

    foldering

--- iCourseStore/iCourseStore.Core.DAL/CourseStoreDB/CourseStoreContextFactory.cs
+++ iCourseStore/iCourseStore.Core.DAL/CourseStoreDB/CourseStoreContextFactory.cs
@@ -8,7 +8,7 @@ public class CourseStoreContextFactory : IDesignTimeDbContextFactory<CourseStore
     public CourseStoreDbContext CreateDbContext(string[] args)
     {
         var optionBuilder = new DbContextOptionsBuilder<CourseStoreDbContext>();
-        optionBuilder.UseSqlServer("Server=.;Initial Catalog=S27E02iCourseStore_DB;User Id=dbuser;Password=1qaz!QAZ;");
+        optionBuilder.UseSqlServer("Server=.;Initial Catalog=S27E02.iCourseStore_DB;User Id=dbuser;Password=1qaz!QAZ;");
         CourseStoreDbContext ctx = new CourseStoreDbContext(optionBuilder.Options);
         return ctx;
     }
