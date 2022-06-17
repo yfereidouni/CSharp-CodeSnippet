61122569b3e544f9a471561d6d38e51076edb82d
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-17 19:38:33 +0430

    role

---
+++ iIdentity/iIdentity.S22E01.Samples.MVC/Controllers/RolesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iIdentity.S22E01.Samples.MVC.Controllers;
+
+public class RolesController : Controller
+{
+    private readonly RoleManager<IdentityRole> roleManager;
+
+    public RolesController(RoleManager<IdentityRole> roleManager)
+    {
+        this.roleManager = roleManager;
+    }
+    public IActionResult Index()
+    {
+        var roles = roleManager.Roles.ToList();
+        return View(roles);
+    }
+}
