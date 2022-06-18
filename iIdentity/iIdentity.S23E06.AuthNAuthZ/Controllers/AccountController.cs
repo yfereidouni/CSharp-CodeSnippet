8c64de1be1cdb09aefb8b5bd60ec721607cee4c3
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-19 00:31:09 +0430

    signInManager

---
+++ iIdentity/iIdentity.S23E06.AuthNAuthZ/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using iIdentity.S23E06.AuthNAuthZ.Models.AAA.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iIdentity.S23E06.AuthNAuthZ.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string redirectUrl = "/")
+        {
+            var model = new LoginModel() { RedirectUrl = redirectUrl };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(model.RedirectUrl);
+                }
+
+                ModelState.AddModelError("", "Invalid Username or Password!");
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ iIdentity/iIdentity.S23E06.AuthNAuthZ/Models/AAA/Data/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace iIdentity.S23E06.AuthNAuthZ.Models.AAA.Data;
+
+public class LoginModel
+{
+    //[Required]
+    public string UserName { get; set; }
+    //[Required]
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+    public string RedirectUrl { get; set; }
+}
