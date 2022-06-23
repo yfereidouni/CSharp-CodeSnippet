f68e7bfd1e0c2205f33b4199254c4e5bb9ddb686
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-06-24 00:35:22 +0430

    Duende Identity Server was configured.

---
+++ iIdentity/S24E19.NET5/iIdentity.S24E19.NET5.FirstClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+
+namespace iIdentity.S24E19.NET5.FirstClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            
+            Console.WriteLine(tokenResponse.Json);
+            Console.ReadLine();
+
+
+            ///Call API
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/api/Identity");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ iIdentity/S24E19.NET5/iIdentity.S24E19.NET5.IdentityServer/Config.cs
@@ -0,0 +1,43 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
+
+namespace iIdentity.S24E19.NET5.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("api1","First API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    ClientSecrets ={new Secret("secret".Sha256())},
+                    AllowedScopes = {"api1"},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials
+                }
+                ,new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = {new Secret ("secret".Sha256()) },
+                    AllowedGrantTypes= GrantTypes.Code,
+
+                    RedirectUris = {"https://localhost:5001/sign-in-oidc"},
+                    PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc"},
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                }
+            };
+    }
+}
